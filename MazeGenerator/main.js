983ba41b5e5543d4336c4abae9f0794e818fe86f
MOhan <31mohan31@gmail.com>
2019-02-21 11:04:27 +0530

    moving traker along the path

--- MazeGenerator/main.js
+++ MazeGenerator/main.js
@@ -1,4 +1,4 @@
-let w = 50;
+let w = 20;
 let c;
 let grids = [];
 let row,col;
@@ -25,20 +25,24 @@ class cell{
         line(x,y+w,x ,y);}
 
         if(this.visited){
-            fill(255,0,255,100);
+            fill(255,0,255,50);
+            noStroke();
             rect(this.x*w,this.y*w,w,w);
         }
     }
 
     index(i,j){
-        if(i< 0 || j < 0|| i < row-1||j<col-1){
+        if(i< 0 || j < 0|| i > row-1||j>col-1){
             return -1;
         }
         return j +i *row;
     }
     checkNeighbour(){
         var neighbour = [];
-
+        // print(this.index(this.x,this.y-1));
+        // print(this.index(this.x+1,this.y));
+        // print(this.index(this.x,this.y+1));
+        // print(this.index(this.x-1,this.y));
         var top = grids[this.index(this.x,this.y-1)];
         var right = grids[this.index(this.x+1,this.y)];
         var bottom = grids[this.index(this.x,this.y+1)];
@@ -59,9 +63,11 @@ class cell{
 
         if(neighbour.length > 0 ){
             var rnd = floor(random(0,neighbour.length));
-            return neighbour[rnd];
+            neighbour[rnd].visited = true;
+                //print(i);
+            return  neighbour[rnd];
         }
-        print(grids);
+        //print(grids);
     }
    
     
@@ -77,21 +83,69 @@ function setup(){
             grids.push(new cell(i,j));
         }
     }
-    fill(50);
+    //fill(50);
     current = grids[0];
     current.visited = true;
+    frameRate(10);
 }
 
+function checkDirection(current,next){
 
+    var xdiff = current.x - next.x;
+    var ydiff = current.y - next.y;
+    if(xdiff > 0){
+        current.sides[3] = false;
+        next.sides[1] = false;
+    }
+    else if(xdiff < 0){
+        current.sides[1] = false;
+        next.sides[3] = false;
+    }
+    if(ydiff < 0){
+        current.sides[2] = false;
+        next.sides[0] = false;
+    }else if(ydiff > 0){
+        current.sides[0] = false;
+        next.sides[2] = false;
+    }
+    // if(current.x == next.x){
+    //     if(current.y - next.y > 0){
+    //         current.sides[0] = false;
+    //         next.sides[2] = false;
+    //         return [[false,true,true,true,],[true,true,false,true]];
+    //     }
+    //     else{
+    //         return [[true,true,false,true],[false,true,true,true]];
+    //     }
+    // }
+    // else{
+    //     if(current.x - next.x > 0){
+    //         return [[true,true,true,false],[true,false,true,true]];
+    //     }
+    //     else{
+    //         return [[true,false,true,true],[true,true,true,false]];
+    //     }
+    // }
+}
 
 function draw(){
+    background(125);
     for(var i = 0; i<grids.length;i++){
         grids[i].show();
-        var next = grids[i].checkNeighbour();  
-        if(next){
-            next.visited = true;
-        }
-        //break;
     }
-    //noLoop();
+    
+    var next = current.checkNeighbour();
+    if(next){
+        
+        var direction = checkDirection(current,next);
+        print(direction)
+        // current.sides = direction[0]
+        // next.sides = direction[1];
+        // current.show();
+        // next.show();
+        //next.show();
+        current = next;
+        
+    }
+    
 }
